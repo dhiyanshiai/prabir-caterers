63724afa78b0cb6ab94a83619e4498352d42c27e
Saurav <sauravganguly338@gmail.com>
2026-03-31 20:08:01 +0530

    Set up ESLint 9 flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypeScript from 'eslint-config-next/typescript';
+
+const config = [
+  ...nextCoreWebVitals,
+  ...nextTypeScript,
+  {
+    ignores: ['tsconfig.tsbuildinfo'],
+  },
+];
+
+export default config;
